815f6c1126453ad029bf5c0ac5103d7d669a4eae
kingdido999 <info@desmonding.me>
2018-11-27 17:44:07 +0800

    Read and write post list

---
+++ posts/hello-world.md
@@ -0,0 +1,15 @@
+# Hello, 世界！
+
+[Rust](https://www.rust-lang.org) is a systems programming language that runs blazingly fast, prevents segfaults, and guarantees thread safety.
+
+## Features
+
+- zero-cost abstractions
+- move semantics
+- guaranteed memory safety
+- threads without data races
+- trait-based generics
+- pattern matching
+- type inference
+- minimal runtime
+- efficient C bindings
--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,37 @@
 extern crate tera;
 
 use pulldown_cmark::{html, Parser};
+use std::fs;
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
+use std::io::Result;
 use std::path::Path;
 use tera::Context;
 
-fn main() -> io::Result<()> {
-    let path = "posts/issue-1.md";
-    let html_buf = convert_markdown_to_html(&path)?;
-
+fn main() -> Result<()> {
     let mut tera = compile_templates!("templates/**/*");
     tera.autoescape_on(vec![]);
 
-    let mut context = Context::new();
-    context.insert("content", &html_buf);
-    let rendered_html = tera.render("layouts/post.html", &context).unwrap();
+    for entry in fs::read_dir("posts")? {
+        let path = entry?.path();
+        let html_buf = convert_markdown_to_html(&path)?;
 
-    let output_path = format!("dist/{}", &path);
-    let mut write_buf = File::create(&output_path)?;
-    write_buf.write(rendered_html.as_bytes())?;
+        let mut context = Context::new();
+        context.insert("content", &html_buf);
+        let rendered_html = tera.render("layouts/post.html", &context).unwrap();
+
+        let output_path = format!(
+            "dist/posts/{}.html",
+            &path.file_stem().unwrap().to_str().unwrap()
+        );
+        let mut write_buf = File::create(&output_path)?;
+        write_buf.write(rendered_html.as_bytes())?;
+    }
 
     Ok(())
 }
 
-fn convert_markdown_to_html<P: AsRef<Path>>(path: P) -> io::Result<String> {
+fn convert_markdown_to_html<P: AsRef<Path>>(path: P) -> Result<String> {
     let mut f = File::open(path)?;
     let mut md_buf = String::new();
     f.read_to_string(&mut md_buf)?;
