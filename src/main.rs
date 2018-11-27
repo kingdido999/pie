dec3fb8c5b073e0e3f6fb446f765ee47b448a099
kingdido999 <info@desmonding.me>
2018-11-27 14:19:39 +0800

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,35 @@ use pulldown_cmark::{html, Parser};
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
+use std::path::Path;
 use tera::Context;
 
 fn main() -> io::Result<()> {
-    let mut f = File::open("posts/issue-1.md")?;
-    let mut buffer = String::new();
-    f.read_to_string(&mut buffer)?;
-
-    let parser = Parser::new(&buffer);
-
-    let mut html_buf = String::new();
-    html::push_html(&mut html_buf, parser);
+    let path = "posts/issue-1.md";
+    let html_buf = convert_markdown_to_html(&path)?;
 
     let mut tera = compile_templates!("templates/**/*");
     tera.autoescape_on(vec![]);
 
     let mut context = Context::new();
-    context.insert("post", &html_buf);
-    let rendered_html = tera.render("index.html", &context).unwrap();
+    context.insert("content", &html_buf);
+    let rendered_html = tera.render("layouts/post.html", &context).unwrap();
 
-    let mut write_buf = File::create("dist/posts/issue-1.html")?;
+    let output_path = format!("dist/{}", &path);
+    let mut write_buf = File::create(&output_path)?;
     write_buf.write(rendered_html.as_bytes())?;
 
     Ok(())
+}
+
+fn convert_markdown_to_html<P: AsRef<Path>>(path: P) -> io::Result<String> {
+    let mut f = File::open(path)?;
+    let mut md_buf = String::new();
+    f.read_to_string(&mut md_buf)?;
+
+    let parser = Parser::new(&md_buf);
+    let mut html_buf = String::new();
+    html::push_html(&mut html_buf, parser);
+
+    Ok(html_buf)
 }
